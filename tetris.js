60eea53ffee2c49c991aa3d651d407b0b1b1ffbb
myGithub123# <royandugu99009@gmail.com>
2022-09-22 09:05:46 +0545

    Future display/Next select recursion

--- tetris.js
+++ tetris.js
@@ -5,11 +5,11 @@ for(let i=0;i<=199;++i){
     gridContainer.appendChild(div);
 }
 
-
-//Game constants
 const grids=document.querySelectorAll(".gridContainer div");
 const width=10;
+const dWidth=4;
 const futureContainer=document.querySelector(".futureContainer");
+
 for(let i=0;i<16;i++){
   const div=document.createElement("div");
   div.textContent=i;
@@ -17,8 +17,7 @@ for(let i=0;i<16;i++){
 }
 const futureGrids=document.querySelectorAll(".futureContainer div");
 
-
-//Tetrimino testing
+//Tetrimino list
 const lTetromino = [
   [1, width+1, width*2+1,2],
   [width, width+1, width+2, width*2+2],
@@ -52,20 +51,27 @@ const iTetromino = [
 const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
 
+//Future display
+const futureTetriminoes=[
+  [1, dWidth+1, dWidth*2+1,2],
+  [0,dWidth,dWidth+1,dWidth*2+1],
+  [1,dWidth,dWidth+1,dWidth+2],
+  [0,1,dWidth,dWidth+1],
+  [1,dWidth+1,dWidth*2+1,dWidth*3+1],
+]
+
+
 //Variables
 let gameEnded=false;
 let blockList=[];
+let futureList=[];
 let direction=0;
 let touchGround=false;
 let choosenPattern;
+let futurePattern;
 let rCount=0;
-let choosenTetrimino;
-let randomIndex=[];
+let firstRandomIndex,secondRandomIndex;
 let i,j;
-
-randomIndex[0]=Math.floor(Math.random()*5);
-randomIndex[1]=Math.floor(Math.random()*5);
-
 
 
 //Listen to keyboard entries
@@ -74,10 +80,98 @@ const keyListen=(e)=>{
   else if(e.keyCode===39) moveTetriminoRight();
   else if(e.keyCode===82) rotateTetrimino();
 }
-
 document.addEventListener("keydown",keyListen);
 
 
+//Display and remove the tetris blocks
+const setCurrentTetrimino=()=>{
+  blockList.forEach(index=>grids[index].classList.add("block"));
+}
+const setFutureTetrimino=()=>{
+  futurePattern.forEach(index=>futureGrids[index].classList.add("futureBlock"));
+}
+const removeCurrentTetrimino=()=>{
+  blockList.forEach(index=>grids[index].classList.remove("block"));
+}
+const removeFutureTetrimino=()=>{
+  futurePattern.forEach(index=>futureGrids[index].classList.remove("futureBlock"));
+}
+
+
+//Select the tetris block and display them
+const selectTetrimino=()=>{
+  touchGround=false;
+  blockList=[];
+  if(secondRandomIndex) firstRandomIndex=secondRandomIndex;
+  else firstRandomIndex=Math.floor(Math.random()*5);
+  secondRandomIndex=Math.floor(Math.random()*5);
+  choosenPattern=theTetrominoes[firstRandomIndex];
+  futurePattern=futureTetriminoes[secondRandomIndex];
+  showTetrimino();
+}
+
+const showTetrimino=()=>{
+  choosenPattern[rCount].forEach((index,i)=>blockList[i]=index);
+  setCurrentTetrimino();
+  setFutureTetrimino();
+  setTimeout(()=>moveTetriminoDown(),500);
+}
+
+
+//Basic movement functionalities
+const moveTetriminoLeft=()=>{
+  if(blockList.some(index=>index%width===0) || blockList.some(index=>grids[index].classList.contains("restedBlock"))) return;
+  removeCurrentTetrimino();
+  blockList=blockList.map(index=>{
+    return index-1; 
+  });
+  setCurrentTetrimino();
+}
+
+const moveTetriminoRight=()=>{
+  if(blockList.some(index=>index%width===width-1) || blockList.some(index=>grids[index].classList.contains("restedBlock"))) return;
+  removeCurrentTetrimino();
+  blockList=blockList.map(index=>{
+    return index+1;
+  })
+  setCurrentTetrimino();
+}
+
+const moveTetriminoDown=()=>{
+  blockList.forEach(index=>{
+    if(index<=189){
+      if(grids[index+width].classList.contains("restedBlock")){
+        touchGround=true;
+        return;
+      }
+      else return;
+    }
+    else{
+      touchGround=true;
+      return;
+    }
+  })
+  if(touchGround){
+    assignRestBlocks();
+    return;
+  }
+  removeCurrentTetrimino();
+  blockList=blockList.map(index=>{
+    return index+width;
+  })
+  setCurrentTetrimino();
+  setTimeout(moveTetriminoDown,500);
+}
+
+const rotateTetrimino=()=>{
+  removeCurrentTetrimino();
+  rCount=(rCount+1)%4;
+  choosenPattern[rCount].forEach((index,i)=>blockList[i]=index);
+  setCurrentTetrimino();
+}
+
+
+//Blocks when they are not falling 
 const assignRestBlocks=()=>{
   if(blockList.length>0){
     blockList.forEach(index=>{
@@ -85,103 +179,8 @@ const assignRestBlocks=()=>{
       grids[index].classList.add("restedBlock");
     })
   }
+  removeFutureTetrimino();
+  selectTetrimino();
   return;
 }
-
-
-const selectTetrimino=()=>{
-  rCount=0;
-  touchGround=false;
-  blockList=[];
-  randomIndex[0]=Math.floor(Math.random()*5);
-  randomIndex[1]=Math.floor(Math.random()*5);
-  choosenPattern=theTetrominoes[randomIndex[0]];
-  showTetrimino();
-}
-
-
-const showTetrimino=()=>{
-  choosenPattern[rCount].forEach((index,i)=>blockList[i]=index);
-  setTetrimino();
-  setTimeout(()=>moveTetriminoDown(),500);
-}
-
-
-const setTetrimino=()=>{
-  blockList.forEach(index=>grids[index].classList.add("block"));
-}
-
-
-const removeTetrimino=()=>{
-  blockList.forEach(index=>grids[index].classList.remove("block"));
-}
-
-
-const moveTetriminoLeft=()=>{
-  if(blockList.some(index=>index%width===0) || blockList.some(index=>grids[index].classList.contains("restedBlock"))) return;
-  removeTetrimino();
-  blockList=blockList.map(index=>{
-    return index-1; 
-  });
-  setTetrimino();
-}
-
-
-const moveTetriminoRight=()=>{
-  if(blockList.some(index=>index%width===width-1) || blockList.some(index=>grids[index].classList.contains("restedBlock"))) return;
-  removeTetrimino();
-  blockList=blockList.map(index=>{
-    return index+1;
-  })
-  setTetrimino();
-}
-
-
-const moveTetriminoDown=()=>{
-  if(blockList.some(index=>grids[index].classList.contains("restedBlock") || index>189)) return;
-  removeTetrimino();
-  blockList=blockList.map(index=>{
-    return index+width;
-  })
-  setTetrimino();
-  setTimeout(moveTetriminoDown,500);
-}
-
-
-const rotateTetrimino=()=>{
-  removeTetrimino();
-  rCount+=rCount%4;
-  choosenPattern[rCount].forEach((index,i)=>blockList[i]=index);
-  showTetrimino();
-}
-
-
-
-// const moveTetrimino=()=>{
-//     //Convert to loop logic rather than recursion
-//     blockList.forEach(index=>{
-//       if(index<=189){
-//         if(grids[index+width].classList.contains("restedBlock"))touchGround=true;
-//       }
-//       else touchGround=true;
-//     })
-//     if(touchGround) {
-//       assignRestBlocks();
-//       selectTetrimino();
-//       return;
-//     }
-//     else{
-//     blockList.forEach(indx=>{
-//       grids[indx].classList.remove("block");
-//     });
-//     for(i=0;i<choosenPattern.length;i++){
-//       choosenPattern[i]=choosenPattern[i].map(indx=>indx+width+direction);  
-//     }
-//     choosenPattern[rCount].forEach((indx,i)=>blockList[i]=indx);
-//     direction=0;
-//     blockList.forEach(indx=>grids[indx].classList.add("block"));
-//     setTimeout(()=>moveTetrimino(blockList),500);
-//   }
-// }
-
 selectTetrimino();
